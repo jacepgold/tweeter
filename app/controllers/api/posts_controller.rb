332c7d17699783de43016068f94c073d61c34ec0
Bryan Leanoe <bryan.leano@gmail.com>
2017-12-16 11:10:34 -0700

    add post_controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,13 +1,43 @@
 class Api::PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post, only: [:show, :update, :destroy]
+  
   def index
+    render json: Post.all
   end
 
   def show
+    render json: @post
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :create
+    else 
+      render json: @post.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @post.destroy
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:content)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
