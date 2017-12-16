fc9a4f6c78049255dd055d3371722d82ea490ace
Austin Phillips <phillips.austin51@gmail.com>
2017-12-16 11:02:57 -0700

    Comment controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,15 +1,43 @@
 class Api::CommentsController < ApplicationController
+  before_action :set_comments, only: [:show, :update, :destroy]
+
   def index
+    render json: Comment.all
   end
 
   def show
+    render json: @comments
   end
 
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessible_entity
+    end 
+  end 
+
   def update
+    if @comment.update(comment_params)
+      render json: @comment
+    else 
+      render json: @comment.errors, status: :unprocessible_entity
+
   end
 
   def destroy
+    @comment.destroy
   end
 
   private
+
+  def set_comments
+    @comments = Comment.find(params[:id])
+  end 
+
+  def comment_params
+    params.require(:comment).permit(:message)
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
-    resources :user, :post, :comment, :bio
+    resources :users, :post, :comment, :bio
   end
 
   #Do not place any routes below this one
