649123815cbb6025674764a20fd1a3a271538217
Bryan Leanoe <bryan.leano@gmail.com>
2017-12-16 17:16:19 -0700

    fix bug in code

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,6 @@ import { combineReducers } from 'redux';
 import user from './user';
 import flash from './flash';
 import tweets from './tweets'
-
-const rootReducer = combineReducers({
-  user,
-  flash,
-  tweets
 import bio from './bio';
 
 const rootReducer = combineReducers({
